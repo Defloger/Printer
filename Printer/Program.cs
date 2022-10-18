23cb80fb0a4c05ad8ded02e4c5be1b06cb27801a
161PC <161PC@DESKTOP-GKJ0FR7>
2022-10-18 17:33:30 +0300

    Добавьте файлы проекта.

---
+++ Printer/Program.cs
@@ -0,0 +1,8 @@
+﻿using Printer;
+PrinterClass printer = new PrinterClass(100, 10000, 50);
+printer.TurnOn();
+printer.Test();
+printer.GetInfo();
+printer.Test();
+printer.AddInk();
+printer.TurnOff();
